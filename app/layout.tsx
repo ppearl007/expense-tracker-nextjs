086b88e2ea09315ba3462267e2ce99839dca87ba
Pelu Adejugba <pelu.adejugba@gmail.com>
2024-11-22 19:39:01 -0800

    add transaction action

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +35,7 @@ export default function RootLayout({
             <Header />
             {children}
           </main>
+          <ToastContainer />
         </body>
       </html>
     </ClerkProvider>
--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,21 +1,31 @@
 'use client'
 
+import addTransaction from "@/actions/addTransaction"
+import {toast} from 'react-toastify'
+
 const AddTransaction = () => {
     const clientAction = async (formData: FormData) => {
-        console.log(formData.get('text'), formData.get('amount'))
+        // console.log(formData.get('text'), formData.get('amount'))
+        const result = await addTransaction(formData)
+
+        if (result.error) {
+            toast.error(result.error)
+        } else {
+            console.log(result.data)
+        }
     }
-    
+
     return (
         <div>
             <h3> AddTransaction</h3>
-            <form action={ clientAction }>
+            <form action={clientAction}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" id="text" name="text" placeholder="Enter text ..."/>
+                    <input type="text" id="text" name="text" placeholder="Enter text ..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount <br /> (negative - expense, positive - income) </label>
-                    <input type="number" id="amount" name="amount" placeholder="Enter amount ..." step="0.01"/>
+                    <input type="number" id="amount" name="amount" placeholder="Enter amount ..." step="0.01" />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
